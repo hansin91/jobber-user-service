179d814af66099ed5436e1a7df84a1ecb8198ebd
hansin91 <hansinsusatya@gmail.com>
2024-02-16 18:31:38 +0700

    interface direct message, update package

--- src/queues/producer.ts
+++ src/queues/producer.ts
@@ -1,18 +1,10 @@
-import { winstonLogger } from '@hansin91/jobber-shared';
+import { DirectMessage, winstonLogger } from '@hansin91/jobber-shared';
 import { config } from '@users/config';
 import { Channel } from 'amqplib';
 import { Logger } from 'winston';
 import { createConnection } from '@users/queues/connection';
 
 const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'usersServiceProducer', 'debug');
-
-export interface DirectMessage {
-  channel: Channel
-  exchangeName: string
-  routingKey: string
-  message: string
-  logMessage: string
-}
 
 export const publishDirectMessage = async (payload: DirectMessage): Promise<void> => {
   let {channel} = payload;
